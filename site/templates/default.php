cea1f3685d77e9879224c07416b0b054444c1bb8
spidergon <spidergon@gmail.com>
2020-12-18 00:08:45 -0300

    projects grid

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,8 +1,8 @@
 <?php snippet('header') ?>
 
-  <main class="main">
-    <h1><?= $page->title() ?></h1>
-    <?= $page->text() ?>
-  </main>
+<main class="main">
+  <h1><?= $page->title() ?></h1>
+  <p><?= $page->text() ?></p>
+</main>
 
 <?php snippet('footer') ?>
--- site/templates/projects.php
+++ site/templates/projects.php
@@ -1,15 +1,20 @@
 <?php snippet('header') ?>
 
-  <main class="main">
-    <h1><?= $page->title() ?></h1>
+<main class="main">
+  <h1><?= $page->title() ?></h1>
 
-    <nav class="menu">
-      <ul>
-        <?php foreach ($page->children()->listed() as $project): ?>
-          <li><a href="<?= $project->url() ?>"><?= $project->title() ?></a></li>
-        <?php endforeach ?>
-      </ul>
-    </nav>
-  </main>
+  <ul class="projects">
+    <?php foreach ($page->children()->listed() as $project): ?>
+      <li>
+        <a href="<?= $project->url() ?>">
+          <figure>
+            <?= $project->image()->crop(400, 500) ?>
+            <figcaption><?= $project->title() ?></figcaption>
+          </figure>
+        </a>
+      </li>
+    <?php endforeach ?>
+  </ul>
+</main>
 
 <?php snippet('footer') ?>
