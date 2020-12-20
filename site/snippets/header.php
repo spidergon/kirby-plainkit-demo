8ff93ad175195e5226355781333e76fff6286cc6
spidergon <spidergon@gmail.com>
2020-12-20 10:39:25 -0300

    titles

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,7 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $site->title() ?></title>
+
+  <?php if ($page->isHomePage()): ?>
+    <title><?= $site->title() ?></title>
+  <?php else: ?>
+    <title><?= $page->seoTitle()->or($page->title()) ?> | <?= $site->title() ?></title>
+  <?php endif ?>
+
   <?= css('assets/css/index.css') ?>
   <?= css('@auto') ?>
 </head>
