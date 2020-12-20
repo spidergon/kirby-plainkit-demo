b122e4ea58c4b885d9c852cd70823a6a294e40d2
spidergon <spidergon@gmail.com>
2020-12-20 12:41:08 -0300

    pagination

--- site/templates/project.php
+++ site/templates/project.php
@@ -7,47 +7,44 @@
     <div class="project-layout">
       <div class="project-info">
         <div class="project-text">
-        <?php if ($page->text()->isNotEmpty()): ?>
-          <p><?= $page->text() ?></p>
+          <?php if ($page->text()->isNotEmpty()) : ?>
+            <p><?= $page->text() ?></p>
           <?php endif ?>
         </div>
 
         <dl>
-          <?php if ($page->year()->isNotEmpty()): ?>
-          <dt>Year</dt>
-          <dd><?= $page->year() ?></dd>
+          <?php if ($page->year()->isNotEmpty()) : ?>
+            <dt>Year</dt>
+            <dd><?= $page->year() ?></dd>
           <?php endif ?>
 
-          <?php if ($page->client()->isNotEmpty()): ?>
-          <dt>Client</dt>
-          <dd><?= $page->client() ?></dd>
+          <?php if ($page->client()->isNotEmpty()) : ?>
+            <dt>Client</dt>
+            <dd><?= $page->client() ?></dd>
           <?php endif ?>
 
-          <?php if ($page->category()->isNotEmpty()): ?>
-          <dt>Category</dt>
-          <dd><?= $page->category() ?></dd>
+          <?php if ($page->category()->isNotEmpty()) : ?>
+            <dt>Category</dt>
+            <dd><?= $page->category() ?></dd>
           <?php endif ?>
 
-          <?php if ($page->link()->isNotEmpty()): ?>
-          <dt>Link</dt>
-          <dd><?= $page->link() ?></dd>
+          <?php if ($page->link()->isNotEmpty()) : ?>
+            <dt>Link</dt>
+            <dd><?= $page->link() ?></dd>
           <?php endif ?>
         </dl>
       </div>
       <div class="project-gallery">
         <ul>
-          <?php foreach($page->images() as $image): ?>
-          <li>
-            <figure>
-              <a href="<?= $image->url() ?>">
-                <img
-                  src="<?= $image->resize(1200, 1200)->url() ?>"
-                  alt="<?= $image->alt() ?>"
-                >
-              </a>
-              <figcaption><?= $image->caption() ?></figcaption>
-            </figure>
-          </li>
+          <?php foreach ($page->images() as $image) : ?>
+            <li>
+              <figure>
+                <a href="<?= $image->url() ?>">
+                  <img src="<?= $image->resize(1200, 1200)->url() ?>" alt="<?= $image->alt() ?>">
+                </a>
+                <figcaption><?= $image->caption() ?></figcaption>
+              </figure>
+            </li>
           <?php endforeach ?>
         </ul>
       </div>
--- site/templates/projects.php
+++ site/templates/projects.php
@@ -1,10 +1,15 @@
 <?php snippet('header') ?>
 
+<?php
+$projects = $page->children()->listed()->paginate(3);
+$pagination = $projects->pagination();
+?>
+
 <main class="main">
   <h1><?= $page->title() ?></h1>
 
   <ul class="projects">
-    <?php foreach ($page->children()->listed() as $project): ?>
+    <?php foreach ($projects as $project) : ?>
       <li>
         <a href="<?= $project->url() ?>">
           <figure>
@@ -15,6 +20,25 @@
       </li>
     <?php endforeach ?>
   </ul>
+
+  <?php if ($pagination->hasPages()) : ?>
+    <nav class="pagination">
+      <?php if ($pagination->hasPrevPage()) : ?>
+        <a href="<?= $pagination->prevPageUrl() ?>" aria-label="Go to previous page">&larr;</a>
+      <?php else : ?>
+        <span>&larr;</span>
+      <?php endif ?>
+
+      <span>Page <?= $pagination->page() ?> of <?= $pagination->pages() ?></span>
+
+      <?php if ($pagination->hasNextPage()) : ?>
+        <a href="<?= $pagination->nextPageUrl() ?>" aria-label="Go to next page">&rarr;</a>
+      <?php else : ?>
+        <span>&rarr;</span>
+      <?php endif ?>
+    </nav>
+  <?php endif ?>
+
 </main>
 
 <?php snippet('footer') ?>
